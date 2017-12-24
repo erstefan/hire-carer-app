527014aa330e0635083a55aa991a8cbbac2c1a7f
Stefan Rusu <erstefan13@gmail.com>
2017-12-24 20:36:06 +0000

    users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('profile_completed');
             $table->rememberToken();
             $table->timestamps();
         });
