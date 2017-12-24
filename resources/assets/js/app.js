2938bcaa6dd522373724ed267a383fb793f78f5e
Stefan Rusu <erstefan13@gmail.com>
2017-12-24 09:13:42 +0000

    removing UI stuff from laravel

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,8 @@
-import Vue from 'vue';
-import App from './components/App';
 
-const app = new Vue({
-    el: '#app',
-    render: app => app(App)
-});
+/**
+ * First, we will load all of this project's Javascript utilities and other
+ * dependencies. Then, we will be ready to develop a robust and powerful
+ * application frontend using useful Laravel and JavaScript libraries.
+ */
+
+require('./bootstrap');
